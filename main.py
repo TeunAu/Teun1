7bb3ad61aab1b7cedca6f5062c75bd2435a4df78
teunaugustus <teunaugustus@gmail.com>
2023-09-25 15:02:17 +0200

    Teun

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-leeftijd = input("hoe oud ben je? ")
-paspoort = input("heb je een nederlands paspoort? ")
-if paspoort == "jadf"
-    print("je mag stemmen")
+import csv
+import datetime
+import random
+
+stations = ["Arnhem, Almere, Amersfoort, Almelo, Alkmaar, Apeldoorn, Assen, Amsterdam, Boxtel, Breda, Dordrecht, Delft, Deventer, Enschede, Gouda, Groningen, Den Haag, Hengelo, Haarlem, Helmond, Hoorn, Heerlen, Den Bosch, Hilversum, Leiden, Lelystad, Leeuwarden, Maastricht, Nijmegen, Oss, Roermond, Roosendaal, Sittard, Tilburg, Utrecht, Venlo, Vlissingen, Zaandam, Zwolle, Zutphen"]
+
+def save_message(message, username=None):
+    if username is None:
+        username = "anoniem"
+
+
+
+
